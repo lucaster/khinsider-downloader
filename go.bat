2ecb45fc337f326cf439374d94907881ad0aa844
Luca <luca.cavagnoli@gmail.com>
2026-05-29 22:32:02 +0200

    playlist

--- go.bat
+++ go.bat
@@ -1,11 +1,11 @@
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses-xx-xbox-360-gamerip-2006
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses-xx-original-soundtrack
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/champion-wrestler-arcade
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/metal-gear-solid-2-sons-of-liberty-original-soundtrack
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/metal-gear-solid-ps1-gamerip-1998
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses-2004-ps2-gamerip
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/neo-turf-masters-neo-geo-arcade
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/endorfun-1995-windows
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/death-stranding-2019
-node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/death-stranding-original-score-volume-2
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses-xx-xbox-360-gamerip-2006 --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses-xx-original-soundtrack --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/champion-wrestler-arcade --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/metal-gear-solid-2-sons-of-liberty-original-soundtrack --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/metal-gear-solid-ps1-gamerip-1998 --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses-2004-ps2-gamerip --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/rumble-roses --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/neo-turf-masters-neo-geo-arcade --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/endorfun-1995-windows --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/death-stranding-2019 --playlist
+node download_khinsider.js https://downloads.khinsider.com/game-soundtracks/album/death-stranding-original-score-volume-2 --playlist
